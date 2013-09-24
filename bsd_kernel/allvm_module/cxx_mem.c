ebce47b7422812ef853db62fb2506d525fa663b5
Will Dietz <w@wdtz.org>
2013-09-24 17:23:51 -0500

    Actually use alloc/dealloc, add debug prints to them.

--- bsd_kernel/allvm_module/cxx_mem.c
+++ bsd_kernel/allvm_module/cxx_mem.c
@@ -14,6 +14,7 @@
 #include <sys/param.h>  /* defines used in kernel.h */
 #include <sys/kernel.h> /* types used in module initialization */
 #include <sys/malloc.h>
+#include <sys/systm.h>
 
 //===-- Primary Allocation ------------------------------------------------===//
 
@@ -21,10 +22,12 @@ MALLOC_DECLARE(allvm_cpp_mem);
 MALLOC_DEFINE(allvm_cpp_mem,"allvm_cpp","Memory for ALLVM C++ kernel objects");
 
 static inline void* alloc(unsigned long sz) {
+  printf("[ALLVM] alloc(%lu)\n", sz);
   return malloc(sz, allvm_cpp_mem, M_ZERO|M_NOWAIT);
 }
 
 static inline void dealloc(void *p) {
+  printf("[ALLVM] dealloc(%p)\n", p);
   return free(p, allvm_cpp_mem);
 }
 
@@ -33,31 +36,31 @@ static inline void dealloc(void *p) {
 // operator new[](unsigned long)
 void* _Znam(unsigned long sz);
 void* _Znam(unsigned long sz) {
-  return malloc(sz, allvm_cpp_mem, M_ZERO|M_NOWAIT);
+  return alloc(sz);
 }
 // operator new(unsigned long)
 void *_Znwm(unsigned long sz);
 void *_Znwm(unsigned long sz) {
-  return malloc(sz, allvm_cpp_mem, M_ZERO|M_NOWAIT);
+  return alloc(sz);
 }
 // operator new(unsigned long, std::nothrow_t const&)
 void *_ZnwmRKSt9nothrow_t(unsigned long sz, int unused);
 void *_ZnwmRKSt9nothrow_t(unsigned long sz, int unused) {
-  return malloc(sz, allvm_cpp_mem, M_ZERO|M_NOWAIT);
+  return alloc(sz);
 }
 
 // operator delete[](void*)
 void _ZdaPv(void *p);
 void _ZdaPv(void *p) {
-  free(p, allvm_cpp_mem);
+  dealloc(p);
 }
 // operator delete(void*)
 void _ZdlPv(void *p);
 void _ZdlPv(void *p) {
-  free(p, allvm_cpp_mem);
+  dealloc(p);
 }
 // operator delete(void*, std::nothrow_t const&)
 void _ZdlPvRKSt9nothrow_t(void *p, int unused);
 void _ZdlPvRKSt9nothrow_t(void *p, int unused) {
-  free(p, allvm_cpp_mem);
+  dealloc(p);
 }
