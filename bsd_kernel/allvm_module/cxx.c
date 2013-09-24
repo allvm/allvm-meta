6e9f09f86e70f6366a1c2bc512d6c21b704021c1
Will Dietz <w@wdtz.org>
2013-09-24 16:41:58 -0500

    Add stubs for required libstdc++ functions!

---
+++ bsd_kernel/allvm_module/cxx.c
@@ -0,0 +1,67 @@
+//===-- cxx.c -------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// C++ stubs
+//
+//===----------------------------------------------------------------------===//
+
+#include "stubs.h"
+
+UNSUPPORTED(_ZNKSs16find_last_not_ofEcm);
+UNSUPPORTED(_ZNKSs4findEPKcmm);
+UNSUPPORTED(_ZNKSs4findEcm);
+UNSUPPORTED(_ZNKSs7compareEPKc);
+UNSUPPORTED(_ZNKSs7compareEmmPKc);
+UNSUPPORTED(_ZNKSs7compareEmmPKcm);
+UNSUPPORTED(_ZNKSt15basic_stringbufIcSt11char_traitsIcESaIcEE3strEv);
+UNSUPPORTED(_ZNSolsEi);
+UNSUPPORTED(_ZNSs12_M_leak_hardEv);
+UNSUPPORTED(_ZNSs14_M_replace_auxEmmmc);
+UNSUPPORTED(_ZNSs4_Rep10_M_disposeERKSaIcE);
+UNSUPPORTED(_ZNSs4_Rep11_S_terminalE);
+UNSUPPORTED(_ZNSs4_Rep20_S_empty_rep_storageE);
+UNSUPPORTED(_ZNSs4_Rep9_S_createEmmRKSaIcE);
+UNSUPPORTED(_ZNSs6appendEPKcm);
+UNSUPPORTED(_ZNSs6appendERKSs);
+UNSUPPORTED(_ZNSs6appendEmc);
+UNSUPPORTED(_ZNSs6assignEPKcm);
+UNSUPPORTED(_ZNSs6assignERKSs);
+UNSUPPORTED(_ZNSs6resizeEmc);
+UNSUPPORTED(_ZNSs7replaceEmmPKcm);
+UNSUPPORTED(_ZNSs7reserveEm);
+UNSUPPORTED(_ZNSs9_M_mutateEmmm);
+UNSUPPORTED(_ZNSsC1EPKcRKSaIcE);
+UNSUPPORTED(_ZNSsC1EPKcmRKSaIcE);
+UNSUPPORTED(_ZNSsC1ERKSs);
+UNSUPPORTED(_ZNSsC1EmcRKSaIcE);
+UNSUPPORTED(_ZNSsD1Ev);
+UNSUPPORTED(_ZNSt6localeC1Ev);
+UNSUPPORTED(_ZNSt6localeD1Ev);
+UNSUPPORTED(_ZNSt8ios_baseC2Ev);
+UNSUPPORTED(_ZNSt8ios_baseD2Ev);
+UNSUPPORTED(_ZNSt9basic_iosIcSt11char_traitsIcEE4initEPSt15basic_streambufIcS1_E);
+UNSUPPORTED(_ZNSt9basic_iosIcSt11char_traitsIcEE5clearESt12_Ios_Iostate);
+UNSUPPORTED(_ZSt16__ostream_insertIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_PKS3_l);
+UNSUPPORTED(_ZSt17__throw_bad_allocv);
+UNSUPPORTED(_ZSt18_Rb_tree_decrementPKSt18_Rb_tree_node_base);
+UNSUPPORTED(_ZSt18_Rb_tree_decrementPSt18_Rb_tree_node_base);
+UNSUPPORTED(_ZSt18_Rb_tree_incrementPKSt18_Rb_tree_node_base);
+UNSUPPORTED(_ZSt18_Rb_tree_incrementPSt18_Rb_tree_node_base);
+UNSUPPORTED(_ZSt19__throw_logic_errorPKc);
+UNSUPPORTED(_ZSt20__throw_length_errorPKc);
+UNSUPPORTED(_ZSt20__throw_out_of_rangePKc);
+UNSUPPORTED(_ZSt28_Rb_tree_rebalance_for_erasePSt18_Rb_tree_node_baseRS_);
+UNSUPPORTED(_ZSt29_Rb_tree_insert_and_rebalancebPSt18_Rb_tree_node_baseS0_RS_);
+UNSUPPORTED(_ZSt7nothrow);
+UNSUPPORTED(_ZTVSt15basic_streambufIcSt11char_traitsIcEE);
+UNSUPPORTED(_ZTVSt15basic_stringbufIcSt11char_traitsIcESaIcEE);
+UNSUPPORTED(_ZTVSt19basic_ostringstreamIcSt11char_traitsIcESaIcEE);
+UNSUPPORTED(__cxa_guard_acquire);
+UNSUPPORTED(__cxa_guard_release);
+UNSUPPORTED(__cxa_pure_virtual);
