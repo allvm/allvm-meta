0df892dfcbc6dcf393cffafcd8d47ed062b56582
Will Dietz <w@wdtz.org>
2013-09-23 19:19:21 -0500

    Add icxxabi.{h,cpp} from OSDev tutorial.
    See:
    
    http://wiki.osdev.org/C%2B%2B

--- bsd_kernel/allvm_module/main.c
+++ bsd_kernel/allvm_module/main.c
@@ -10,6 +10,9 @@
 #include <sys/param.h>  /* defines used in kernel.h */
 #include <sys/kernel.h> /* types used in module initialization */
 
+// CXX support
+#include "icxxabi.h"
+
 extern int testJIT();
 
 /*
