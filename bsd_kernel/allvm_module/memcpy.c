8652883a8a4393cc3da29bc5138d22c93a64c9e9
Will Dietz <w@wdtz.org>
2013-10-01 11:58:22 -0500

    Intercept memcpy

---
+++ bsd_kernel/allvm_module/memcpy.c
@@ -0,0 +1,23 @@
+//===-- memcpy.c ----------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// memcpy wrapper
+//
+//===----------------------------------------------------------------------===//
+
+#include "common.h"
+
+void *__real_memcpy(void *dest, const void *src, size_t n);
+
+void *__wrap_memcpy(void *dest, const void *src, size_t n);
+void *__wrap_memcpy(void *dest, const void *src, size_t n) {
+  printf("memcpy(dest=%p, src=%p, n=%zu)\n", dest, src, n);
+  return __real_memcpy(dest, src, n);
+}
+
