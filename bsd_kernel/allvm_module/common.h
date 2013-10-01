52022dcfc39ff366af30b95453754392c882d8d5
Will Dietz <w@wdtz.org>
2013-10-01 10:50:15 -0500

    Redirect printf/vprintf through our own versions.

--- bsd_kernel/allvm_module/common.h
+++ bsd_kernel/allvm_module/common.h
@@ -16,12 +16,13 @@
 
 #include <sys/types.h>
 #include <sys/module.h>
-#include <sys/systm.h> /* uprintf */
+
+void panic(const char *, ...) __dead2 __printflike(1, 2);
+int printf(const char *, ...) __printflike(1, 2);
 
 static inline const char* nullstr(const char *str) {
   return str ? str : "<null>";
 }
-
 
 //#define BAIL() panic("Unsupported call to '%s'!\n", __FUNCTION__)
 #define BAIL() printf("Unsupported call to '%s'!\n", __FUNCTION__)
@@ -30,6 +31,6 @@ static inline const char* nullstr(const char *str) {
   void name(void); \
   void name() { BAIL(); }
 
-#define TRACE() printf("[TRACE] Call to '%s'\n", __FUNCTION__)
+#define TRACE() printf("<TRACE> Call to '%s'\n", __FUNCTION__)
 
 #endif // _COMMON_H_
---
+++ bsd_kernel/allvm_module/printf.c
@@ -0,0 +1,53 @@
+//===-- printf.c ----------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// Redirect implementation for printf
+//
+//===----------------------------------------------------------------------===//
+
+#include <sys/types.h>
+#include <sys/module.h>
+#include <sys/systm.h>
+
+#include <machine/stdarg.h>
+
+int __real_printf(const char *, ...) __printflike(1, 2);
+int	__real_vprintf(const char *, __va_list) __printflike(1, 0);
+
+
+static const size_t FORMAT_LEN = 1 << 12;
+static char buf[FORMAT_LEN];
+static const char* prefix = "[ALLVM] ";
+
+static const char* add_prefix(const char *fmt) {
+  if ((strlcpy(buf, prefix, FORMAT_LEN) > FORMAT_LEN) ||
+      (strlcat(buf, fmt, FORMAT_LEN) > FORMAT_LEN)) {
+    __real_printf("[ALLVM] Format specifier overflow! :(\n");
+    return 0;
+  }
+  return buf;
+}
+
+int __wrap_printf(const char * fmt, ...);
+int __wrap_printf(const char * fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = -1;
+  if ((fmt = add_prefix(fmt)))
+    ret = __real_vprintf(fmt, ap);
+  va_end(ap);
+  return ret;
+}
+
+int __wrap_vprintf(const char* fmt, __va_list ap);
+int __wrap_vprintf(const char* fmt, __va_list ap) {
+  if ((fmt = add_prefix(fmt)))
+    return __real_vprintf(fmt, ap);
+  return -1;
+}
