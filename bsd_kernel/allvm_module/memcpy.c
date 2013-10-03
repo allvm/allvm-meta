e4b36b2385fd1b0d6310cae377c6fcd251045a73
Will Dietz <w@wdtz.org>
2013-10-03 15:04:55 -0500

    don't log memcpy any longer

--- bsd_kernel/allvm_module/memcpy.c
+++ bsd_kernel/allvm_module/memcpy.c
@@ -17,7 +17,7 @@ void *__real_memcpy(void *dest, const void *src, size_t n);
 
 void *__wrap_memcpy(void *dest, const void *src, size_t n);
 void *__wrap_memcpy(void *dest, const void *src, size_t n) {
-  printf("memcpy(dest=%p, src=%p, n=%zu)\n", dest, src, n);
+  // printf("memcpy(dest=%p, src=%p, n=%zu)\n", dest, src, n);
   return __real_memcpy(dest, src, n);
 }
 
